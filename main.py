aa532584b76a6e661756e865a72ab6ba44b75b5d
Vegan Cheese <110762965+vegan-cheese@users.noreply.github.com>
2025-12-03 14:36:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
-from astro_pi_orbit import ISS
-import math as m
+import astro_pi_orbit
+import math
 import time
 
-iss = ISS()
+from skyfield.api import load, EarthSatellite, Loader
+import numpy as np
+
+iss = astro_pi_orbit.ISS()
 point = iss.coordinates()
 prev_height = point.elevation.km
 prev_lat = point.latitude.radians
 prev_long = point.longitude.radians
-def haversine(x1, x2):
-    return m.sin((x1 - x2) / 2) ** 2
 
-def Haversine_formula(radius, coord1, coord2):
-    angle = m.asin((haversine(coord1[0], coord2[0]) + m.cos(coord1[0]) * m.cos(coord2[0]) * haversine(coord1[1], coord2[1]) ** 0.5))
-    return 2 * radius * angle
+def haversine(coord1, coord2, radius):
+    a = (math.sin((coord2[0] - coord1[0]) / 2))**2 + math.cos(coord1[0]) * math.cos(coord2[0]) * (math.sin((coord2[1] - coord1[1]) / 2)**2)
+    distance = 2 * radius * math.asin(math.sqrt(a))
+    return distance
+
+def get_radius(latitude):
+    a = 6378.137
+    b = 6356.7523412
+    deglat = math.degrees(latitude)
+    radius = math.sqrt((((a**2)*math.cos(latitude))**2 + ((b**2)*(math.sin(latitude)))**2) / ((a*math.cos(latitude))**2 + (b * math.sin(latitude))**2))
+    print(radius)
+    return radius
 
 
-while True:
+start_time = time.time()
+while time.time() < start_time + 5:
     time.sleep(1)
-    
+
     point = iss.coordinates()
     curr_height = point.elevation.km
     curr_lat = point.latitude.radians
     curr_long = point.longitude.radians
-    print(Haversine_formula(6370 + ((prev_height + curr_height) / 2), (prev_lat, prev_long), (curr_lat, curr_long)))
-    
+
+    radius = get_radius((prev_lat + curr_lat) / 2) + ((prev_height + curr_height) / 2)
+    speed = haversine((prev_lat, prev_long), (curr_lat, curr_long), radius)
+    print(speed)
+
     prev_height = curr_height
     prev_lat = curr_lat
     prev_long = curr_long
-    print(prev_lat * (180 / m.pi))
